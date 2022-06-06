f55da79734f68ab99f666302ef1aa825e2a34141
JAMAL EDDINE ELIDRISSI Yassir <67668097+JAMALEEY@users.noreply.github.com>
2022-06-06 12:17:00 +0100

    Add AuthController and its dto (registerRequest)

---
+++ src/main/java/ma/youcode/firo/controller/AuthController.java
@@ -0,0 +1,21 @@
+package ma.youcode.firo.controller;
+
+import ma.youcode.firo.dto.RegisterRequest;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+
+
+public class AuthController {
+
+    @PostMapping
+    public void signup(@RequestBody RegisterRequest registerRequest) {
+
+    }
+
+
+}
