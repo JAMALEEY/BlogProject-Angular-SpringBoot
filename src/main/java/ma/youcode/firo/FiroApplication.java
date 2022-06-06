2255ffc24db97eeb35d47f78eb43b8ba0c54a2a8
JAMAL EDDINE ELIDRISSI Yassir <67668097+JAMALEEY@users.noreply.github.com>
2022-06-06 10:11:07 +0100

    project init.

---
+++ src/main/java/ma/youcode/firo/FiroApplication.java
@@ -0,0 +1,13 @@
+package ma.youcode.firo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ma/youcode/firo/FiroApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.youcode.firo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
