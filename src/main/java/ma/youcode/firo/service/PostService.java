1bc5b3d19313b817e5d9e538e24b71b147fc018c
JAMAL EDDINE ELIDRISSI Yassir <67668097+JAMALEEY@users.noreply.github.com>
2022-06-07 21:52:35 +0100

    Add mapFromPostToDto in PostService inside the showAllPosts

--- src/main/java/ma/youcode/firo/service/PostService.java
+++ src/main/java/ma/youcode/firo/service/PostService.java
@@ -8,6 +8,9 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
 
 @Service
 public class PostService {
@@ -31,7 +34,20 @@ public class PostService {
     }
 
 
-    public Object showAllPosts() {
+    public List<PostDto> showAllPosts() {
+        List<Post> posts = postRepository.findAll();
+        return posts.stream().map(this::mapFromPostToDto).collect(toList());
+
+    }
+
+    private PostDto mapFromPostToDto(Post post) {
+        PostDto postDto = new PostDto();
+        postDto.setId(post.getId());
+        postDto.setTitle(post.getTitle());
+        postDto.setContent(post.getContent());
+        postDto.setUsername(post.getUsername());
+        return postDto;
+
     }
 
     public Object readSinglePost(Long id) {
