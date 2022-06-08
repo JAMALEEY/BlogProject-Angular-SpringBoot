32ecd892f0afea81451fc73bc4d2aee135293c03
JAMAL EDDINE ELIDRISSI Yassir <67668097+JAMALEEY@users.noreply.github.com>
2022-06-08 22:41:26 +0100

    Fix Cors bug.

--- src/main/java/ma/youcode/firo/controller/AuthController.java
+++ src/main/java/ma/youcode/firo/controller/AuthController.java
@@ -2,6 +2,7 @@ package ma.youcode.firo.controller;
 
 import ma.youcode.firo.dto.LoginRequest;
 import ma.youcode.firo.dto.RegisterRequest;
+import ma.youcode.firo.model.User;
 import ma.youcode.firo.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -13,25 +14,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/auth")
-
-
 public class AuthController {
-
     // autowire the service
     @Autowired
     private AuthService authService;
-
-
     @PostMapping("/signup")
-    public ResponseEntity signup(@RequestBody RegisterRequest registerRequest) {
+    public ResponseEntity <?> signup(@RequestBody RegisterRequest registerRequest) {
         authService.signup(registerRequest);
         return new ResponseEntity(HttpStatus.OK);
     }
-
     @PostMapping("/login")
     public String login(@RequestBody LoginRequest loginRequest){
        return authService.login(loginRequest);
     }
-
 
 }
