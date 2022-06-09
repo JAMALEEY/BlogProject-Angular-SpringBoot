b65d0b2ac4be3a494bcaaf772bc7c2ddc684ddf6
JAMAL EDDINE ELIDRISSI Yassir <67668097+JAMALEEY@users.noreply.github.com>
2022-06-09 02:18:45 +0100

    Refactor return type to allow jwt and user as respones

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { RegisterPayload } from './auth/register-payload';
 import { LoginPayload } from './auth/login-payload';
+import { JwtAutResponse } from './auth/jwt-aut-response';
+import { LocalStorageService } from 'ngx-webstorage';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +13,8 @@ export class AuthService {
   private url = 'http://localhost:8080/api/auth/';
 
   constructor(
-    private httpClient: HttpClient
+    private httpClient: HttpClient,
+    private localStorageService: LocalStorageService
     ) {
     }
     register(registerPayload: RegisterPayload) :Observable<any>{
@@ -21,10 +24,20 @@ export class AuthService {
         )
     }
 
-    login(loginPayload: LoginPayload){
-      return this.httpClient.post(
+    login(loginPayload: LoginPayload): Observable <boolean> {
+      return this.httpClient.post <JwtAutResponse> (
         this.url + 'login',
         loginPayload 
-      )
+      ).pipe(map(data => {
+          this.localStorageService.store(
+            'authenticationToken',
+            data.AuthenticationToken
+          );
+          this.localStorageService.store(
+            'username',
+            data.username
+          );
+          return true;
+      } ));
     }
 }
---
+++ src/app/auth/jwt-aut-response.ts
@@ -0,0 +1,4 @@
+export class JwtAutResponse {
+    AuthenticationToken?: String;
+    username?: String;
+}
