311b3d5310d5225dc0e52281a2ed28a216c9f199
JAMAL EDDINE ELIDRISSI Yassir <67668097+JAMALEEY@users.noreply.github.com>
2022-06-09 01:48:39 +0100

    Add login component & auth service

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { AuthService } from 'src/app/auth.service';
+import { LoginPayload } from '../login-payload';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+  loginPayload!: LoginPayload;
 
-  constructor() { }
+  constructor(private authService: AuthService) {
+    this.loginForm = new FormGroup(
+      {
+        username: new FormControl(),
+        password: new FormControl()
+      }
+    )
+   }
 
   ngOnInit(): void {
+  }
+  onSubmit(){
+    // BAsically the payload allows us to send the data to backend
+    this.loginPayload.username = this.loginForm.get('username')?.value;
+    this.loginPayload.password = this.loginForm.get('password')?.value;
+
+    //inject oauth service into the component
+    this.authService.login(this.loginPayload)
   }
 
 }
